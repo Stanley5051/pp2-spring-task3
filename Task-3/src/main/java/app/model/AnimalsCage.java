1083c28322dab9236e696beddefe507b629b4a39
Stanley5051 <timofeev-egor90@yandex.ru>
2024-09-25 11:52:05 +0300

    Add files via upload
    initial commit

---
+++ Task-3/src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,26 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("dog") //указываем кнкрэтный бин
+    private Animal animal;
+
+    @Autowired
+    private Timer timer;
+    public Timer getTimer() {
+        return timer; // Добавляем метод для доступа к Timer, чтоб прокатить тесты
+    }
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
+        System.out.println("________________________");
+    }
+}
